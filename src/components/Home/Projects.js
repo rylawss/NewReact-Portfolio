24de53cdd3361a872478e349deab13eb2eb7388d
rylawss <128633609+rylawss@users.noreply.github.com>
2023-09-18 14:07:14 -0500

    images of projects

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -4,12 +4,18 @@ import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import bookSearch from "../../Assets/booksearch.PNG";
+import urllsTavern from "../../Assets/UrllsTavern.PNG";
+import employee from "../../Assets/EmployeeTracker.PNG";
+import ecom from "../../Assets/EcomBackend.PNG";
+import social from "../../Assets/SocialAPI.PNG";
+import portfolio from "../../Assets/Portfolio.PNG";
 
 function Projects() {
   return (
     <Carousel>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={employee} />
         <p className="legend">
           <a href="https://github.com/rylawss/12-Employee-Tracker">
             Employee Tracker
@@ -17,7 +23,7 @@ function Projects() {
         </p>
       </div>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={ecom} />
         <p className="legend">
           <a href="https://github.com/rylawss/13-E-commerce-Back-End">
             E-commerce Back-End
@@ -25,7 +31,7 @@ function Projects() {
         </p>
       </div>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={social} />
         <p className="legend">
           <a href="https://github.com/rylawss/18-Social-Network-API">
             Social Network API
@@ -33,7 +39,7 @@ function Projects() {
         </p>
       </div>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={bookSearch} />
         <p className="legend">
           <a href="https://github.com/rylawss/Book-Search-Engine">
             Book search engine
@@ -41,7 +47,7 @@ function Projects() {
         </p>
       </div>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={urllsTavern} />
         <p className="legend">
           <a href="https://github.com/t3p3-TheConglomerate/UllrsTavern">
             UllrsTavern
@@ -49,7 +55,7 @@ function Projects() {
         </p>
       </div>
       <div>
-        <img src="https://picsum.photos/800/301/?random" />
+        <img src={portfolio} />
         <p className="legend">
           <a href="https://github.com/rylawss/NewReact-Portfolio">
             This sites repo
